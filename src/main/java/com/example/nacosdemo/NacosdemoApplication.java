6717fea45d8b7df8e6c1062b77e992a602c2a19d
suyilin <964498287@qq.com>
2020-12-23 15:21:36 +0800

    憨憨

--- src/main/java/com/example/nacosdemo/NacosdemoApplication.java
+++ src/main/java/com/example/nacosdemo/NacosdemoApplication.java
@@ -1,9 +1,10 @@
 package com.example.nacosdemo;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@NacosPropertySource(dataId = "example", autoRefreshed = true)
 @SpringBootApplication
 public class NacosdemoApplication {
 
---
+++ src/main/java/com/example/nacosdemo/config/ConfigController.java
@@ -0,0 +1,26 @@
+package com.example.nacosdemo.config;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * @author suyilin w011
+ * @date 2020/12/23 14:31
+ * @description
+ */
+@Controller
+@RequestMapping("config")
+public class ConfigController {
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public boolean get() {
+        return useLocalCache;
+    }
+}
